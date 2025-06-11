91be6d1390454181e3695cb036f00788ab0dbd2a
amarkatoliya <amarbharavad994@gmail.com>
2025-06-11 21:51:01 +0530

    07 -react project added

---
+++ 07-react/chai-router/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Chai aur Code</h1>
+    </div>
+  )
+}
+
+export default App
